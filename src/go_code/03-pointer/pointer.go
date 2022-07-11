cdb140a84a290c73b162a50faaeb77807e90d0b9
yorkzhang <zyk961122@gmail.com>
2022-07-11 08:55:07 -0700

    test git push

--- src/go_code/03-pointer/pointer.go
+++ src/go_code/03-pointer/pointer.go
@@ -11,7 +11,7 @@ func main() {
 	// ptr: 指针变量，类型为*int，值为&i
 	var ptr *int = &i
 	fmt.Printf("ptr = %v\n", ptr)
-	// 指针变量本身也有一个地址
+	// 指针变量本身也有地址
 	fmt.Printf("ptr的地址 = %v\n", &ptr)
 
 	// 获取指针指向的值，使用星号*
